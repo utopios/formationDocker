084c8efbb1be98e9a09a1999041ef353378b1b37
ihab <ihab@macbook-pro-de-ihab.home>
2021-06-03 19:22:43 +0200

    suite cours apache 2

--- dockerfiles/serveur-web-node/index.js
+++ dockerfiles/serveur-web-node/index.js
@@ -1,9 +1,10 @@
 const express = require("express")
-
+const http = require("http")
 const app = express()
 
 app.get('/', (req, res) => {
     res.end("Bonjour tout le monde")
+    http.request('http://'+process.env.ADDRESS_APACHE4)
 })
 
 app.listen(80, () => {
